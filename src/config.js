adaab3b7b601c498c6b5b348a2da39f63844a632
Sivanathan V <104622092+Sivav15@users.noreply.github.com>
2022-11-25 15:33:53 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const env = {
-    api : "http://localhost:4000",
-    // api : "https://inventorymanagementz.herokuapp.com"
+//     api : "http://localhost:4000",
+    api : "https://shop-inventory-management-back-21v3jgzve-sivav15.vercel.app"
 }
